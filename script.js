2f1b1ff85bf54e146ce46d1ddefc17100287b263
vemodig <vornehed@gmail.com>
2025-01-16 14:40:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var pigBtn = document.querySelector("#pig-button");
 var horseBtn = document.querySelector("#horse-button");
 var chickenBtn = document.querySelector("#chicken-button");
 var catBtn = document.querySelector("#cat-button");
+var restartBtn = document.querySelector("#restart-button");
+
+var allImages = [];
 
 cowBtn.onclick = function () {
   document.getElementById("cow-sound").play();
@@ -22,8 +25,8 @@ cowBtn.onclick = function () {
       "https://cdn.glitch.global/00ffaf65-2464-4b67-80f5-c035446fb0f9/cow-2-pxl-1.2.png?v=1737022092985";
   }
   document.body.appendChild(cowImg);
+  allImages.push(cowImg);
 };
-
 
 pigBtn.onclick = function () {
   document.getElementById("pig-sound").play();
@@ -43,11 +46,12 @@ pigBtn.onclick = function () {
       "https://cdn.glitch.global/00ffaf65-2464-4b67-80f5-c035446fb0f9/pig-1-pxl-1.2.png?v=1737022099606";
   }
   document.body.appendChild(pigImg);
+  allImages.push(pigImg);
 };
 
 horseBtn.onclick = function () {
   document.getElementById("horse-sound").play();
-    var horseImg = document.createElement("img");
+  var horseImg = document.createElement("img");
   horseImg.style.position = "absolute";
   horseImg.style.left = Math.random() * 100 + "%";
   horseImg.style.top = Math.random() * 100 + "%";
@@ -63,6 +67,7 @@ horseBtn.onclick = function () {
       "https://cdn.glitch.global/00ffaf65-2464-4b67-80f5-c035446fb0f9/horse-2-pxl-1.2.png?v=1737022097060";
   }
   document.body.appendChild(horseImg);
+  allImages.push(horseImg);
 };
 
 chickenBtn.onclick = function () {
@@ -86,8 +91,36 @@ chickenBtn.onclick = function () {
       "https://cdn.glitch.global/00ffaf65-2464-4b67-80f5-c035446fb0f9/chicken-1-pxl-1.2.png?v=1737022079560";
   }
   document.body.appendChild(chickenImg);
+  allImages.push(chickenImg);
 };
 
 catBtn.onclick = function () {
   document.getElementById("cat-sound").play();
+  var catImg = document.createElement("img");
+  catImg.style.position = "absolute";
+  catImg.style.left = Math.random() * 100 + "%";
+  catImg.style.top = Math.random() * 100 + "%";
+  catImg.style.width = 100 + Math.random() * 100 + "px";
+
+  var randomNumber = Math.random();
+
+  if (randomNumber < 0.5) {
+    catImg.src =
+      "https://cdn.glitch.global/00ffaf65-2464-4b67-80f5-c035446fb0f9/cat-2-pxl-1.2.png?v=1737027248394";
+  } else {
+    catImg.src =
+      "https://cdn.glitch.global/00ffaf65-2464-4b67-80f5-c035446fb0f9/cat-1-pxl-1.2.png?v=1737027250366";
+  }
+  document.body.appendChild(catImg);
+  allImages.push(catImg);
+};
+
+restartBtn.onclick = function () {
+  document.getElementById("restart-sound").play();
+
+  allImages.forEach(function (img) {
+    img.remove();
+  });
+
+  allImages = [];
 };
